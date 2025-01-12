9759852261aee5e9129eeb88e44931ec6fc1b8d5
Radoslav Ivanov <beepboopitsthejoop@gmail.com>
2025-01-12 15:37:38 +0200

    Simplified math equations

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,30 @@
 #include "ray.h"
 #include "vec3.h"
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+double hit_sphere(const point3& center, double radius, const ray& r) {
+     vec3 oc = center - r.origin();
+     auto a = r.direction().length_squared();
+     auto h = dot(r.direction(), oc);
+     auto c = oc.length_squared() - radius * radius;
+     auto discriminant = h * h - a * c;
+
+     if (discriminant < 0) {
+          return -1.0;
+     } else {
+          return (h - std::sqrt(discriminant)) / a;
+     }
 }
 
 color ray_color(const ray& r) {
-     if (hit_sphere(point3(0,0,-1), 0.5, r))
-          return color(1, 0, 0);
+     auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+     if (t > 0.0) {
+          vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+          return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+     }
 
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+     vec3 unit_direction = unit_vector(r.direction());
+     auto a = 0.5 * (unit_direction.y() + 1.0);
+     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main() {
@@ -36,10 +44,12 @@ int main() {
 
      auto focal_length = 1.0;
      auto viewport_height = 2.0;
-     auto viewport_width = viewport_height * (double(image_width) / image_height);
+     auto viewport_width =
+         viewport_height * (double(image_width) / image_height);
      auto camera_center = point3(0, 0, 0);
 
-     // Calculate the vectors across the horizontal and down the vertical viewport edges.
+     // Calculate the vectors across the horizontal and down the vertical
+     // viewport edges.
      auto viewport_u = vec3(viewport_width, 0, 0);
      auto viewport_v = vec3(0, -viewport_height, 0);
 
@@ -48,8 +58,10 @@ int main() {
      auto pixel_delta_v = viewport_v / image_height;
 
      // Calculate the location of the upper left pixel.
-     auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+     auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) -
+                                viewport_u / 2 - viewport_v / 2;
+     auto pixel00_loc =
+         viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
      // P3 * ASCII
      // 256 256 * Col Row
@@ -57,10 +69,12 @@ int main() {
      std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
      for (int row = 0; row < image_height; row++) {
-          std::clog << "\rScanlines remaining: " << (image_height - row) << ' ' << std::flush;
+          std::clog << "\rScanlines remaining: " << (image_height - row) << ' '
+                    << std::flush;
 
           for (int col = 0; col < image_width; col++) {
-               auto pixel_center = pixel00_loc + (col * pixel_delta_u) + (row * pixel_delta_v);
+               auto pixel_center =
+                   pixel00_loc + (col * pixel_delta_u) + (row * pixel_delta_v);
                auto ray_direction = pixel_center - camera_center;
                ray r(camera_center, ray_direction);
 

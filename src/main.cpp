c10d044026d6cc177dcf9b4796dbe0809368c0d3
Radoslav Ivanov <beepboopitsthejoop@gmail.com>
2025-01-12 16:25:23 +0200

    Added hittable class; Added determination of the side a surface is hit from at geometry time; refactored project with rtweekend.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,13 @@
-#include <iostream>
+#include "rtweekend.h"
 
-#include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-     vec3 oc = center - r.origin();
-     auto a = r.direction().length_squared();
-     auto h = dot(r.direction(), oc);
-     auto c = oc.length_squared() - radius * radius;
-     auto discriminant = h * h - a * c;
-
-     if (discriminant < 0) {
-          return -1.0;
-     } else {
-          return (h - std::sqrt(discriminant)) / a;
-     }
-}
-
-color ray_color(const ray& r) {
-     auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-     if (t > 0.0) {
-          vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-          return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+color ray_color(const ray& r, const hittable& world) {
+     hit_record rec;
+     if (world.hit(r, 0, infinity, rec)) {
+          return 0.5 * (rec.normal + color(1, 1, 1));
      }
 
      vec3 unit_direction = unit_vector(r.direction());
@@ -40,12 +25,18 @@ int main() {
      int image_height = int(image_width / aspect_ratio);
      image_height = (image_height < 1) ? 1 : image_height;
 
+     // World
+
+     hittable_list world;
+
+     world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
      // Camera
 
      auto focal_length = 1.0;
      auto viewport_height = 2.0;
-     auto viewport_width =
-         viewport_height * (double(image_width) / image_height);
+     auto viewport_width = viewport_height * (double(image_width) / image_height);
      auto camera_center = point3(0, 0, 0);
 
      // Calculate the vectors across the horizontal and down the vertical
@@ -58,10 +49,8 @@ int main() {
      auto pixel_delta_v = viewport_v / image_height;
 
      // Calculate the location of the upper left pixel.
-     auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) -
-                                viewport_u / 2 - viewport_v / 2;
-     auto pixel00_loc =
-         viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+     auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
      // P3 * ASCII
      // 256 256 * Col Row
@@ -78,9 +67,9 @@ int main() {
                auto ray_direction = pixel_center - camera_center;
                ray r(camera_center, ray_direction);
 
-               color pixel_color = ray_color(r);
+               color pixel_color = ray_color(r, world);
                write_color(std::cout, pixel_color);
           }
      }
-     std::clog << "\rDone.                 \n";
+     // std::clog << "\rDone.                 \n";
 }

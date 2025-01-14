841135b744915cc46d03f078f50abe98a9668567
Radoslav Ivanov <beepboopitsthejoop@gmail.com>
2025-01-14 14:55:01 +0200

    Implemented lambertian material with always scattering lambertian reflectance

--- include/material.h
+++ include/material.h
@@ -4,14 +4,33 @@
 #include "hittable.h"
 
 class material {
-  public:
-    virtual ~material() = default;
+    public:
+     virtual ~material() = default;
 
-    virtual bool scatter(
-        const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
-    ) const {
-        return false;
-    }
+     virtual bool scatter(const ray& r_in, const hit_record& rec,
+                          color& attenuation, ray& scattered) const {
+          return false;
+     }
+};
+
+class lambertian : public material {
+    public:
+     lambertian(const color& albedo) : albedo(albedo) {}
+
+     bool scatter(const ray& r_in, const hit_record& rec, color& attenuation,
+                  ray& scattered) const override {
+          auto scatter_direction = rec.normal + random_unit_vector();
+
+          // Catch degenerate scatter direction
+          if (scatter_direction.near_zero()) scatter_direction = rec.normal;
+
+          scattered = ray(rec.p, scatter_direction);
+          attenuation = albedo;
+          return true;
+     }
+
+    private:
+     color albedo;
 };
 
 #endif

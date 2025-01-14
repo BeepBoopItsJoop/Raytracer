9d3e330df1e9c874d490e85827accb6d2de70ccd
Radoslav Ivanov <beepboopitsthejoop@gmail.com>
2025-01-14 16:16:56 +0200

    Added metal material; changed scene to different materials with different colors

--- include/material.h
+++ include/material.h
@@ -19,12 +19,31 @@ class lambertian : public material {
 
      bool scatter(const ray& r_in, const hit_record& rec, color& attenuation,
                   ray& scattered) const override {
+          // Random diffuse model
+          // vec3 direction = random_on_hemisphere(rec.normal);
           auto scatter_direction = rec.normal + random_unit_vector();
 
           // Catch degenerate scatter direction
           if (scatter_direction.near_zero()) scatter_direction = rec.normal;
 
           scattered = ray(rec.p, scatter_direction);
+          attenuation = albedo;
+          return true;
+     }
+
+    private:
+     color albedo;
+};
+
+class metal : public material {
+    public:
+     metal(const color& albedo) : albedo(albedo) {}
+
+     bool scatter(const ray& r_in, const hit_record& rec, color& attenuation,
+                  ray& scattered) const override {
+          vec3 reflected = reflect(r_in.direction(), rec.normal);
+
+          scattered = ray(rec.p, reflected);
           attenuation = albedo;
           return true;
      }

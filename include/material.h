a145831ef95594f3d245acd800b1f9d968eb6c31
Radoslav Ivanov <beepboopitsthejoop@gmail.com>
2025-01-15 14:43:39 +0200

    Dielectric material sometimes reflects, sometimes refracts; added air bubble in water sphere

--- include/material.h
+++ include/material.h
@@ -37,7 +37,8 @@ class lambertian : public material {
 
 class metal : public material {
     public:
-     metal(const color& albedo, double fuzz) : albedo(albedo), fuzz(fuzz < 1 ? fuzz : 1) {}
+     metal(const color& albedo, double fuzz)
+         : albedo(albedo), fuzz(fuzz < 1 ? fuzz : 1) {}
 
      bool scatter(const ray& r_in, const hit_record& rec, color& attenuation,
                   ray& scattered) const override {
@@ -46,7 +47,7 @@ class metal : public material {
 
           scattered = ray(rec.p, reflected);
           attenuation = albedo;
-          
+
           return (dot(scattered.direction(), rec.normal) > 0);
      }
 
@@ -56,25 +57,38 @@ class metal : public material {
 };
 
 class dielectric : public material {
-  public:
-    dielectric(double refraction_index) : refraction_index(refraction_index) {}
+    public:
+     dielectric(double refraction_index) : refraction_index(refraction_index) {}
 
-    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
-    const override {
-        attenuation = color(1.0, 1.0, 1.0);
-        double ri = rec.front_face ? (1.0/refraction_index) : refraction_index;
+     bool scatter(const ray& r_in, const hit_record& rec, color& attenuation,
+                  ray& scattered) const override {
+          attenuation = color(1.0, 1.0, 1.0);
+          double ri =
+              rec.front_face ? (1.0 / refraction_index) : refraction_index;
 
-        vec3 unit_direction = unit_vector(r_in.direction());
-        vec3 refracted = refract(unit_direction, rec.normal, ri);
+          vec3 unit_direction = unit_vector(r_in.direction());
+          double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+          double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
 
-        scattered = ray(rec.p, refracted);
-        return true;
-    }
+          bool cannot_refract = ri * sin_theta > 1.0;
+          vec3 direction;
 
-  private:
-    // Refractive index in vacuum or air, or the ratio of the material's refractive index over
-    // the refractive index of the enclosing media
-    double refraction_index;
+          if (cannot_refract)
+               direction = reflect(unit_direction, rec.normal);
+          else
+               direction = refract(unit_direction, rec.normal, ri);
+
+          scattered = ray(rec.p, direction);
+          //    vec3 refracted = refract(unit_direction, rec.normal, ri);
+
+          //    scattered = ray(rec.p, refracted);
+          return true;
+     }
+
+    private:
+     // Refractive index in vacuum or air, or the ratio of the material's
+     // refractive index over the refractive index of the enclosing media
+     double refraction_index;
 };
 
 #endif
